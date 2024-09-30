6f9565c98b55ff5e3c25f63574d687328123c604
Sushant Singh <sushantbibhu@gmail.com>
2024-09-30 16:14:58 +0530

    "Added Seguridad component to Home page"

---
+++ src/app/components/Seguridad.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+interface SecurityItemProps {
+  icon: string;
+  title: string;
+}
+
+const SecurityItem: React.FC<SecurityItemProps> = ({ icon, title }) => (
+  <div className="flex flex-col items-center p-4 bg-white rounded-lg shadow-md">
+    <Image src={icon} alt={title} width={64} height={64} className="mb-2" />
+    <h3 className="text-center font-semibold">{title}</h3>
+  </div>
+);
+
+export default function Seguridad() {
+  return (
+    <section className="p-4 bg-gray-100">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold mb-4 text-center">Seguridad</h2>
+        <p className="text-base mb-6 text-center max-w-2xl mx-auto">
+          Trabajando en infraestructura desarrollada por AWS, Reli está
+          construido para apegarse a los estándares de seguridad más completos.
+          Así blindamos tus datos.
+        </p>
+        <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4">
+          <SecurityItem
+            icon="/placeholder.svg?height=64&width=64"
+            title="NOM 151"
+          />
+          <SecurityItem
+            icon="/placeholder.svg?height=64&width=64"
+            title="ISO-27000"
+          />
+          <SecurityItem
+            icon="/placeholder.svg?height=64&width=64"
+            title="SCO2"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Seguridad from "./components/Seguridad";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Seguridad />
       <Footer />
     </div>
   );
