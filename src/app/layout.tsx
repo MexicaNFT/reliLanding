d897ae3fa48354754de287649a3f0b7480d100a2
Sushant Singh <sushantbibhu@gmail.com>
2024-10-08 02:14:20 +0530

    "Refactor Header component, update navigation logic, and reorganize layout structure"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        {children}
+        <div className="sticky top-0 z-50">
+          <Header />
+        </div>
+        <div className="flex-grow">{children}</div>
+        <Footer />
       </body>
     </html>
   );
