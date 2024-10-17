cd230646c5ffe2ad57c5507222ad227d49378641
Pablo Banzo <pridapablo@gmail.com>
2024-10-18 00:29:19 +0200

    remove initial state logic

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import { components } from "../components/AuthComponents";
 import Subscriptions from "../components/Subscriptions";
-import { useSearchParams } from 'next/navigation';
 
 Amplify.configure(outputs);
 
 export default function Subscription() {
-  const searchParams = useSearchParams();
-  const [authState, setAuthState] = useState<"signIn" | "signUp">("signUp");
-
-  useEffect(() => {
-    if (searchParams.get('state') === 'signIn') {
-      setAuthState('signIn');
-    }
-  }, [searchParams]);
-
   return (
     <Authenticator.Provider>
       <div className="h-screen flex items-center justify-center">
-        <Authenticator initialState={authState} components={components}>
+        <Authenticator initialState={"signUp"} components={components}>
           <Subscriptions />
         </Authenticator>
       </div>
