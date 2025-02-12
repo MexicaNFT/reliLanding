b79c2494697e9826e1ca5ed6900a453ae73890fc
Pablo Banzo <pridapablo@gmail.com>
2025-02-12 12:37:11 -0600

    Refactor Subscription component to dynamically set initial authentication state based on URL parameters

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
@@ -8,13 +9,33 @@ import outputs from "@/amplify_outputs.json";
 import { components } from "../components/AuthComponents";
 import Subscriptions from "../components/Subscriptions";
 
+// Configure Amplify once at the top level of your app.
 Amplify.configure(outputs);
 
-export default function Subscription() {
+export default function SubscriptionPage() {
+  const searchParams = useSearchParams();
+  const [initialState, setInitialState] = useState<
+    "signUp" | "signIn" | "forgotPassword" | null
+  >(null);
+
+  React.useEffect(() => {
+    // Type assertion to ensure stateParam is one of the allowed values
+    const stateParam = (searchParams.get("state") || "signUp") as
+      | "signUp"
+      | "signIn"
+      | "forgotPassword";
+    setInitialState(stateParam);
+  }, [searchParams]);
+
+  // Don't render anything (or optionally show a loading indicator) until we know the initialState
+  if (!initialState) {
+    return null; // or <p>Loading...</p>
+  }
+
   return (
     <Authenticator.Provider>
       <div className="h-screen flex items-center justify-center">
-        <Authenticator initialState={"signUp"} components={components}>
+        <Authenticator initialState={initialState} components={components}>
           <Subscriptions />
         </Authenticator>
       </div>
