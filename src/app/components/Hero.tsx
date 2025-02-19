f55bc791e6185ae0b0da9bc871971f36978fca9c
Sushant Singh <sushantbibhu@gmail.com>
2025-02-19 16:38:41 +0530

    Enhance Hero component with new design and logo integration

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,30 +1,32 @@
 "use client";
 import React from "react";
 import { handleSigninClick } from "../helpers/handle-sign-in";
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="py-10 md:py-20 bg-gradient-to-tr from-[#e6f7f4] via-white via-60% to-[#e6f7f4]">
+    <section className="py-10 md:py-20 bg-gradient-to-b from-white via-[#DCF5F0] via-60% to-[#1ABC9C]">
       <div className="container mx-auto px-4 mt-20">
-        <h1 className="font-poppins font-bold text-3xl sm:text-4xl md:text-5xl lg:text-[58px] leading-tight md:leading-[110px] text-center mb-4 md:mb-6 text-[#36454F] py-4">
-          La plataforma de LegalTech más completa de México
+        <div className="flex justify-center gap-x-1 bg-white p-2 rounded-full w-max mx-auto ">
+          <Image
+            src="/assets/favicon.ico"
+            alt="Reli"
+            width={30}
+            height={30}
+            style={{ objectFit: "contain" }}
+          />
+          <h1 className="text-[#1ABC9C] text-xl font-semibold">
+            Welcome to Reli
+          </h1>
+        </div>
+        <h1 className="font-poppins font-bold text-3xl sm:text-4xl md:text-5xl lg:text-[58px] leading-tight md:leading-[110px] text-center mb-4 md:mb-6 text-[#36454F] py-0">
+          La Solución #1 de Abogados <br />
+          de Bolsillo en México
         </h1>
 
-        <p className="font-poppins font-medium text-base sm:text-lg md:text-2xl text-[#787878] leading-tight md:leading-[42px] text-center mb-6 md:mb-10 max-w-3xl mx-auto">
-          Mejora tus procesos legales usando modelos especializados y a la
-          medida para tu empresa
-        </p>
-
-        <div className="flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-6 mb-24 md:mb-40 my-14">
-          <Link
-            href="/subscription"
-            className="w-48  sm:w-auto font-poppins text-xs sm:text-sm font-normal leading-5 sm:leading-6 px-3 py-2 sm:px-4 sm:py-3 bg-white text-[#36454F] border border-[#36454F] rounded-full hover:bg-gray-100 transition duration-300 text-center"
-          >
-            Sign Up
-          </Link>
+        <div className="flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-6 mb-24 md:mb-40 my-24">
           <button
-            className="w-48 sm:w-auto font-poppins text-xs sm:text-sm font-normal leading-5 sm:leading-6 px-3 py-2 sm:px-4 sm:py-3 bg-[#34C1A6] text-white border border-[#34C1A6] rounded-full hover:bg-[#2ca089] transition duration-300"
+            className="w-48 sm:w-auto font-poppins text-xs sm:text-sm font-normal leading-5 sm:leading-6  py-2 sm:px-8 sm:py-3 bg-[#34C1A6] text-white border border-[#34C1A6] rounded-full hover:bg-[#2ca089] transition duration-300 px-8"
             onClick={handleSigninClick}
           >
             Sign In
