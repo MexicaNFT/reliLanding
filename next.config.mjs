7d035576b9503c6c28b0a84dd27ccf8d8365536f
Alberto Folch <albertofolch@Albertos-MacBook-Air.local>
2026-04-30 17:54:50 -0600

    Migrate deployment from AWS Amplify to Firebase App Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.reli.ai' }],
+        destination: 'https://reli.ai/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
